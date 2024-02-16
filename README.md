8f18b7a5e184fa36e5986552ed21a28ac5accd0d
ddd <daiman4444@gmail.com>
2024-02-16 22:41:28 +0600

    added .env and appconfig package. created mifrations to postgre

--- README.md
+++ README.md
@@ -5,3 +5,20 @@
  - [Scheme](https://miro.com/app/board/uXjVNu7G3NM=/)
  - Desig(future)
  
+  # .env
+ в папке проекта создать файл .env. Он добавлен в .gitgnore и не будет отслеживаться в git. в этом его основная суть.
+ так же в нем находястя переменные и секреты котоыре загружаются в момент старта приложения.
+
+
+   # General settings
+   TIME_ZONE="Asia/Almaty"
+
+   # PostgreSQL
+   PSQL_ENGINE=postgresql
+   PSQL_USER=              # pg username
+   PSQL_PASSWORD=          # pg user password
+   PSQL_HOST=localhost
+   PSQL_NAME=              # pg db name
+   PSQL_PORT=5432
+
+*Для работы с PostgreSQl необходимо предварительно создать пользователя и базу данных
--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+from AppConfig.config import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -81,8 +83,12 @@ WSGI_APPLICATION = 'coolsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": f"django.db.backends.{config.PSQL_ENGINE}",
+        "NAME": config.PSQL_NAME,
+        "USER": config.PSQL_USER,
+        "PASSWORD": config.PSQL_PASSWORD,
+        "HOST": config.PSQL_HOST,
+        "PORT": config.PSQL_PORT,
     }
 }
 
@@ -111,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = config.TIME_ZONE
 
 USE_I18N = True
 
