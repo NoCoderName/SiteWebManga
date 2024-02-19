b042615bc2ca94c6a665a2646673d13f1b50aae8
NoCoderName <134956018+NoCoderName@users.noreply.github.com>
2024-02-19 14:00:31 +0600

    updated the profile model

--- coolsite/ProfileApp/models.py
+++ coolsite/ProfileApp/models.py
@@ -7,6 +7,10 @@ from BlogApp.models import Manga
 class Profile(models.Model):
     photo = models.ImageField(upload_to='photo_user/%Y/%m/%d/', blank=True, null=True, verbose_name='Фото пользователя')
     read = models.ManyToManyField(Manga, blank=True, verbose_name='Читают')
+    read_it = models.ManyToManyField(Manga, blank=True, related_name='read_ it+', verbose_name='Прочитано')
+    favorites = models.ManyToManyField(Manga, blank=True, related_name='favorites+', verbose_name='Избранное')
+    abandoned = models.ManyToManyField(Manga, blank=True, related_name='abandoned+', verbose_name='Брошено')
+    friends = models.ManyToManyField(User, blank=True, related_name='friends+', verbose_name='Друзья')
     user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name='Пользователь')
     
     def __str__(self):
